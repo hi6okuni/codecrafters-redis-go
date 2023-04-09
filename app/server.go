fe0223f75d4967cac9ed4d361daf32026181ddde
hi6okuni <hi6okuni@gmail.com>
2023-04-09 20:54:45 +0900

    feat: concurrent handle

--- app/server.go
+++ app/server.go
@@ -7,6 +7,9 @@ import (
 	"os"
 )
 
+// to check how the server works
+// $ telnet 127.0.0.1 6379
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
@@ -17,12 +20,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
 
+}
+
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	for {
@@ -36,9 +46,9 @@ func main() {
 				os.Exit(1)
 			}
 		}
+
 		// let's ingore the client's inpt for now and hardcode a response.
 		// we'll implement a proper Redis Protocol parse in later stages.
 		conn.Write([]byte("+PONG\r\n"))
 	}
-
 }
